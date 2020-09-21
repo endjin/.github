6007b8250b985a3ec9ddd77ccfb390beb758b275
James Dawson <james@readsource.co.uk>
2020-09-22 00:10:38 +0100

    Fix Pester test failure reporting

--- run-tests.ps1
+++ run-tests.ps1
@@ -7,7 +7,7 @@ try {
     if (!$existingModule -or ($existingModule.Version -notcontains $pesterVer)) {
         Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser -Repository PSGallery
     }
-    Import-Module Pester
+    Import-Module Pester -Version $pesterVer
 
     # Handle any module pre-reqs
     $requiredModules = @(
@@ -23,11 +23,22 @@ try {
     Get-Module Endjin.CodeOps | Remove-Module -Force
     $results = Invoke-Pester $here -PassThru
 
-    if ($results.FailedCount -gt 0) {
-        Write-Host ("{0} out of {1} tests failed - check previous logging for more details" -f $results.FailedCount, $results.TotalCount)
-    }
 
-    $results
+    $total = $results.TotalCount
+    $passed = $results.PassedCount
+    $failed = $results.FailedCount
+    $skipped = $results.SkippedCount
+
+    Write-Host "`nTEST SUMMARY"
+    Write-Host "Total Tests        : $total"
+    Write-Host "Total Passed Tests : $passed"
+    Write-Host "Total Failed Tests : $failed"
+    Write-Host "Total Skipped Tests: $skipped"
+
+    if ($failed -gt 0) {
+        Write-Host "Some tests failed - check previous logs"
+        exit 1
+    }
 }
 catch {
     Write-Output ("::error file={0},line={1},col={2}::{3}" -f `
