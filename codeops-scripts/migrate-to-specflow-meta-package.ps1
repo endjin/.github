e0456a3e2f1975e9dc9927d47af25af44d20020d
James Dawson <james.dawson@endjin.com>
2020-12-03 13:21:30 +0000

    Updated 'migrate-specflow-meta-package' script to continue processing remaining repos after one has an error.

--- codeops-scripts/migrate-to-specflow-meta-package.ps1
+++ codeops-scripts/migrate-to-specflow-meta-package.ps1
@@ -107,9 +107,11 @@ function _repoChanges
 
 function _main
 {
-    try {
-        $repos = Get-AllRepoConfiguration -ConfigDirectory $ConfigDirectory -LocalMode
-        foreach ($repo in $repos) {
+    $failedRepos = @()
+
+    $repos = Get-AllRepoConfiguration -ConfigDirectory $ConfigDirectory -LocalMode
+    foreach ($repo in $repos) {
+        try {
             # When running in GitHub Actions we will need ensure the GitHub App is
             # authenticated for the current GitHub Org
             if ($env:SSH_PRIVATE_KEY -and $env:GITHUB_APP_ID) {
@@ -156,12 +158,20 @@ function _main
                 }
             }
         }
+        catch {
+            # Track the failed repo, before continuing with the rest
+            $failedRepoName = '{0}/{1}' -f $repo.org, $repoName
+            $failedRepos += $failedRepoName
+            WRite-Warning "The repository '$failedRepoName' reporting the following error:"
+            Write-Warning $_.Exception.Message
+            Write-Warning $_.ScriptStackTrace
+            Write-Warning "Processing of remaining repositories will continue"
+        }
     }
-    catch {
-        $ErrorActionPreference = 'Continue'
-        Write-Host "Error: $($_.Exception.Message)"
-        Write-Warning $_.ScriptStackTrace
-        Write-Error $_.Exception.Message
+
+    if ($failedRepos.Count -gt 0) {
+        $ErrorActionPreference = "Continue"
+        Write-Error ("The following repositories reported errors during processing:`n{0}" -f ($failedRepos -join "`n"))
         exit 1
     }
 }
