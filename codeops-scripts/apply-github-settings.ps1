efef800afd47dd6de0a5559c2e09c781432e5509
James Dawson <james.dawson@endjin.com>
2021-03-10 01:02:45 +0000

    Add dependency on Endjin.GitHubActions module

--- codeops-scripts/apply-github-settings.ps1
+++ codeops-scripts/apply-github-settings.ps1
@@ -19,6 +19,7 @@ $here = Split-Path -Parent $PSCommandPath
 # Install other module dependencies
 $requiredModules = @(
     @{ Name = "Endjin.CodeOps"; Version = "0.2.3" }
+    @{ Name = "Endjin.GitHubActions"; Version = "1.0.1" }
 )
 $requiredModules | ForEach-Object {
     if ( !(Get-Module -ListAvailable $_.Name | Where-Object { $_.Version -eq $_.Version }) ) {
