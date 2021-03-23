55f242e2fd902842e58103cddcb287727c0b938f
James Dawson <james.dawson@endjin.com>
2021-03-23 03:11:09 +0000

    debug

--- run-tests.ps1
+++ run-tests.ps1
@@ -2,6 +2,10 @@ $ErrorActionPreference = 'Stop'
 $here = Split-Path -Parent $PSCommandPath
 $pesterVer = '4.10.1'
 try {
+    Get-PackageProvider | ft | out-string | Write-Host
+    Get-PSRepository | ft | Out-String | Write-Host
+    
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $psGallery = Get-PSRepository | Where-Object { $_.SourceLocation -eq "https://www.powershellgallery.com/api/v2" }
     if (!$psGallery) {
         Write-Host "Registering PSGallery"
